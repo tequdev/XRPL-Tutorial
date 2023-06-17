4851283459aaece0b68a3df8c768137b4108eb1a
tequ <git@tequ.dev>
2023-06-17 20:26:05 +0900

    fix autofill

--- src/hooks/useTransactionAutofill.ts
+++ src/hooks/useTransactionAutofill.ts
@@ -6,6 +6,9 @@ export const useTransactionAutofill = () => {
   const client = useXrplClient()
   const autofill = async (txjson: Record<string, unknown>) => {
     await client.connect()
+    delete txjson.LastLedgerSequence
+    delete txjson.Fee
+    delete txjson.Sequence
     const tx = await client.autofill(txjson as unknown as Transaction)
     return tx
   }
